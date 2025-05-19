0c1304571dcf9841e9cab89db4bd8eabd0bf6be6
marselin4 <rodulfcristmas@gmail.com>
2025-05-19 09:14:44 -0300

    Add files via upload

---
+++ nvim/lua/config/options.lua
@@ -0,0 +1,11 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.shiftwidth = 4 
+
+vim.g.mapleader = " "
+
+vim.keymap.set({ "n", "x" },"<C-c>", [["+y]]) -- copy to system clipboard
+
+vim.keymap.set({ "n", "x" }, "<C-v>", [["+p]]) -- paste from system clipboard
+
